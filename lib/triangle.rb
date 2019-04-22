a10b3520cf913846f579d121ca5f2cd54f8d828c
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-04-22 01:11:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  
+  def initialize(length1, length2, length3)
+    @triangle_sides = []
+    @triangle_sides << length1
+    @triangle_sides << length2
+    @triangle_sides << length3
+  end
+
+  def kind
+    if @triangle_sides.uniq.length == 1
+        return :equilateral
+      elsif @triangle_sides.uniq.length == 2
+        return :isosceles
+      else
+        return :scalene
+      end
+    else
+      raise TriangleError
+      end
+    end 
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
